385d14fbf6ed40cca1c95024e945896da8e6aac1
jlmuorizaga <61767859+jlmuorizaga@users.noreply.github.com>
2024-08-04 16:12:27 -0600

    Update conexion_data_db.js

--- conexion_data_db.js
+++ conexion_data_db.js
@@ -1,5 +1,5 @@
-//const DB_HOST = process.env.DB_HOST || 'database-1.cgujpjkz4fsl.us-west-1.rds.amazonaws.com';
- const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_HOST = process.env.DB_HOST || 'database-1.cgujpjkz4fsl.us-west-1.rds.amazonaws.com';
+ //const DB_HOST = process.env.DB_HOST || 'localhost';
  const DB_USER = process.env.DB_USER || 'cheesepizzauser';
  const DB_PASSWORD = process.env.DB_PASSWORD || 'cheesepizza2001';
  const DB_NAME = process.env.DB_NAME || 'chppreciosespecprodpromocdb';
@@ -8,13 +8,13 @@
 
  
  //AWS
- //const DB_PORT = process.env.DB_PORT || 5432;
+ const DB_PORT = process.env.DB_PORT || 5432;
 
  //MacBook
  //const DB_PORT = process.env.DB_PORT || 5432;
 
  //Laptop Omen
-const DB_PORT = process.env.DB_PORT || 5434;
+//const DB_PORT = process.env.DB_PORT || 5434;
 
 //Desktop INEGI
 //const DB_PORT = process.env.DB_PORT || 5433;s

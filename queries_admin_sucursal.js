6bfbe291a9af2a8fa22411d80df9d5e2abf6f102
jlmuorizaga <61767859+jlmuorizaga@users.noreply.github.com>
2024-09-26 00:00:58 -0600

    Update queries_admin_sucursal.js

--- queries_admin_sucursal.js
+++ queries_admin_sucursal.js
@@ -48,15 +48,15 @@ const getSucursal= (request, response) => {
     );
 }
 const insertaSucursal = (req, res) => {
-    const { idSucursal, claveSucursal,nombreSucursal,rfc,domicilio,telefono,horaInicio,horaFin,latitud,longitud,idLugar,ventaActiva,pk,sk,
-        montoMinimoEntregaSucursal,montoMinimoEntregaDomicilio} = req.body;
+    const { id, clave,nombre_sucursal,rfc,domicilio,telefono,hora_inicio,hora_fin,latitud,longitud,id_region,venta_activa,pk,sk,
+        monto_minimo_entrega_sucursal,monto_minimo_entrega_domicilio} = req.body;
     pool.query(
         'INSERT INTO preesppropro.sucursal(id, clave, nombre_sucursal, rfc, domicilio, telefono, hora_inicio, hora_fin, '
-        +'latitud, longitud, id_lugar, venta_activa, pk, sk, monto_minimo_entrega_sucursal, monto_minimo_entrega_domicilio) '
+        +'latitud, longitud, id_region, venta_activa, pk, sk, monto_minimo_entrega_sucursal, monto_minimo_entrega_domicilio) '
         +'VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16) RETURNING *',
         
-        [idSucursal, claveSucursal,nombreSucursal,rfc,domicilio,telefono,horaInicio,horaFin,latitud,longitud,idLugar,ventaActiva,pk,sk,
-            montoMinimoEntregaSucursal,montoMinimoEntregaDomicilio],
+        [id, clave,nombre_sucursal,rfc,domicilio,telefono,hora_inicio,hora_fin,latitud,longitud,id_region,venta_activa,pk,sk,
+            monto_minimo_entrega_sucursal,monto_minimo_entrega_domicilio],
         (error, results) => {
             if (error) {
                 throw error;
@@ -69,14 +69,14 @@ const insertaSucursal = (req, res) => {
 
 const actualizaSucursal= (req, res) => {
     const idSucursal = req.params.idSucursal;
-    const {claveSucursal,nombreSucursal,rfc,domicilio,telefono,horaInicio,horaFin,latitud,longitud,idLugar,ventaActiva,pk,sk,
+    const {claveSucursal,nombreSucursal,rfc,domicilio,telefono,horaInicio,horaFin,latitud,longitud,idRegion,ventaActiva,pk,sk,
         montoMinimoEntregaSucursal,montoMinimoEntregaDomicilio} = req.body;
         console.log('mmed='+montoMinimoEntregaDomicilio);
     pool.query(
         'UPDATE preesppropro.sucursal SET clave=$2, nombre_sucursal=$3, rfc=$4, domicilio=$5, telefono=$6, hora_inicio=$7, hora_fin=$8, '
-        +'latitud=$9, longitud=$10, id_lugar=$11, venta_activa=$12, pk=$13, sk=$14, monto_minimo_entrega_sucursal=$15, monto_minimo_entrega_domicilio=$16 '
+        +'latitud=$9, longitud=$10, id_region=$11, venta_activa=$12, pk=$13, sk=$14, monto_minimo_entrega_sucursal=$15, monto_minimo_entrega_domicilio=$16 '
         +'WHERE id=$1 RETURNING *',
-        [idSucursal,claveSucursal,nombreSucursal,rfc,domicilio,telefono,horaInicio,horaFin,latitud,longitud,idLugar,ventaActiva,pk,sk,
+        [idSucursal,claveSucursal,nombreSucursal,rfc,domicilio,telefono,horaInicio,horaFin,latitud,longitud,idRegion,ventaActiva,pk,sk,
             montoMinimoEntregaSucursal,montoMinimoEntregaDomicilio],
         (error, results) => {
             if (error) {

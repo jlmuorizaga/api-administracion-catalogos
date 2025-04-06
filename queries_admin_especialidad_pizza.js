203c43db30c4508745d8a6d581102caacfef953f
jlmuorizaga <61767859+jlmuorizaga@users.noreply.github.com>
2025-04-05 21:08:31 -0600

    Update queries_admin_especialidad_pizza.js

--- queries_admin_especialidad_pizza.js
+++ queries_admin_especialidad_pizza.js
@@ -32,6 +32,7 @@ const getListaEspecialidades = (request, response) => {
 const getEspecialidad= (request, response) => {
     const idEspecialidad = request.params.idEspecialidad;
     console.log('idEspecialidad='+idEspecialidad);
+    console.log('img_url='+img_url)
     pool.query(
         //'SELECT id as idEspecialidad, nombre as nombreEspecialidad, ingredientes as ingredientesEspecialidad,aplica_2x1 as aplica2x1Especialidad, aplica_p1 as aplicaP1Especialidad FROM preesppropro.especialidad_pizza WHERE id=$1 ORDER by nombre',
         'SELECT id, nombre, ingredientes, img_url, orden,cantidad_ingredientes,es_de_un_ingrediente FROM preesppropro.especialidad_pizza as ep WHERE id=$1 ORDER by nombre',

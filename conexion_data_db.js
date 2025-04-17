4cf863df6baf94ace4e2544e10d8521c368844f3
jlmuorizaga <61767859+jlmuorizaga@users.noreply.github.com>
2025-04-16 20:32:46 -0600

    Update conexion_data_db.js

--- conexion_data_db.js
+++ conexion_data_db.js
@@ -1,12 +1,24 @@
 //const DB_HOST = process.env.DB_HOST || 'database-1.cgujpjkz4fsl.us-west-1.rds.amazonaws.com';
- const DB_HOST = process.env.DB_HOST ||'database-1.czyiomwau3kc.us-east-1.rds.amazonaws.com';
+
+
+//Servidor de prueba AWS Appix Software 16 de abril 2025
+ /*const DB_HOST = process.env.DB_HOST ||'database-1.czyiomwau3kc.us-east-1.rds.amazonaws.com';
  //const DB_HOST = process.env.DB_HOST || 'localhost';
  const DB_USER = process.env.DB_USER || 'cheesepizzauser';
  const DB_PASSWORD = process.env.DB_PASSWORD || 'cheesepizza2001';
  const DB_NAME = process.env.DB_NAME ||  'chppreciosespecprodpromocdb';
  const URL_SERVER=process.env.URL_SERVER || 'http://ec2-54-153-58-93.us-west-1.compute.amazonaws.com/';
  const ENABLE_SSL = true; // Cambia a true para habilitar SSL
+*/
 
+// Servidor AWS de CHP Móvil 16 de abril 2025
+  const DB_HOST = process.env.DB_HOST ||'chp-movil-server.csbwzfd0zlqm.us-east-2.rds.amazonaws.com';
+ //const DB_HOST = process.env.DB_HOST || 'localhost';
+ const DB_USER = process.env.DB_USER || 'postgres';
+ const DB_PASSWORD = process.env.DB_PASSWORD || 'db-chp-movil-2025';
+ const DB_NAME = process.env.DB_NAME ||  'chppreciosespecprodpromocdb';
+ const URL_SERVER=process.env.URL_SERVER || 'http://ec2-54-153-58-93.us-west-1.compute.amazonaws.com/';
+ const ENABLE_SSL = true; // Cambia a true para habilitar SSL
 
  
  //AWS

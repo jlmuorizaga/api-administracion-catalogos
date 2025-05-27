ff5ea67922986b5b4fa507d06cdb1e934674ad56
JOSE LUIS MU ORIZAGA <jlmuo@yahoo.com>
2025-05-27 12:34:34 -0600

    Update conexion_data_db.js

--- conexion_data_db.js
+++ conexion_data_db.js
@@ -2,15 +2,15 @@
 
 
 //Servidor de prueba AWS Appix Software 16 de abril 2025
- /*const DB_HOST = process.env.DB_HOST ||'database-1.czyiomwau3kc.us-east-1.rds.amazonaws.com';
+ const DB_HOST = process.env.DB_HOST ||'database-1.czyiomwau3kc.us-east-1.rds.amazonaws.com';
  //const DB_HOST = process.env.DB_HOST || 'localhost';
  const DB_USER = process.env.DB_USER || 'cheesepizzauser';
  const DB_PASSWORD = process.env.DB_PASSWORD || 'cheesepizza2001';
  const DB_NAME = process.env.DB_NAME ||  'chppreciosespecprodpromocdb';
  const URL_SERVER=process.env.URL_SERVER || 'http://ec2-54-153-58-93.us-west-1.compute.amazonaws.com/';
  const ENABLE_SSL = true; // Cambia a true para habilitar SSL
-*/
 
+/*
 // Servidor AWS de CHP Móvil 16 de abril 2025
   const DB_HOST = process.env.DB_HOST ||'chp-movil-server.csbwzfd0zlqm.us-east-2.rds.amazonaws.com';
  //const DB_HOST = process.env.DB_HOST || 'localhost';
@@ -19,7 +19,7 @@
  const DB_NAME = process.env.DB_NAME ||  'chppreciosespecprodpromocdb';
  const URL_SERVER=process.env.URL_SERVER || 'http://ec2-54-153-58-93.us-west-1.compute.amazonaws.com/';
  const ENABLE_SSL = true; // Cambia a true para habilitar SSL
-
+*/
  
  //AWS
  const DB_PORT = process.env.DB_PORT || 5432;

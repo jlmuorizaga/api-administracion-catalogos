96d9586a85cd13f13e36a517c4f24ceb007023d0
JOSE LUIS MU ORIZAGA <jlmuo@yahoo.com>
2025-05-27 14:43:31 -0600

    Update index_administrador.js

--- index_administrador.js
+++ index_administrador.js
@@ -151,7 +151,7 @@ app.post('/upload', upload.single('image'), (req, res) => {
     return res.status(400).json({ message: 'No se envió ningún archivo' });
   }
 
-  const subcarpeta = req.body.subcarpeta || 'default';
+  const subcarpeta = req.body.subcarpeta || 'promociones';
   console.log('subcarpeta==>',subcarpeta);
   const fileUrl = `http://ec2-54-144-58-67.compute-1.amazonaws.com/img/${subcarpeta}/${req.file.filename}`;
 

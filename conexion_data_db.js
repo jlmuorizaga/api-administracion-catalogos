f54037d588e4be4062816c9cff39dd3d54dddf64
jlmuorizaga <61767859+jlmuorizaga@users.noreply.github.com>
2025-12-07 11:50:07 -0600

    Update conexion_data_db.js

--- conexion_data_db.js
+++ conexion_data_db.js
@@ -2,12 +2,18 @@
 
 
 //Servidor de prueba AWS Appix Software 16 de abril 2025
- const DB_HOST = process.env.DB_HOST ||'database-1.czyiomwau3kc.us-east-1.rds.amazonaws.com';
+ //const DB_HOST = process.env.DB_HOST ||'database-1.czyiomwau3kc.us-east-1.rds.amazonaws.com';
  //const DB_HOST = process.env.DB_HOST || 'localhost';
+ //Servidor AWS CheesePizzaAppix 7 de Diciembre del 2025
+ const DB_HOST = process.env.DB_HOST ||'chp-movil-server.csbwzfd0zlqm.us-east-2.rds.amazonaws.com';
+ 
  const DB_USER = process.env.DB_USER || 'cheesepizzauser';
  const DB_PASSWORD = process.env.DB_PASSWORD || 'cheesepizza2001';
  const DB_NAME = process.env.DB_NAME ||  'chppreciosespecprodpromocdb';
- const URL_SERVER=process.env.URL_SERVER || 'http://ec2-54-144-58-67.compute-1.amazonaws.com/';
+ 
+ //const URL_SERVER=process.env.URL_SERVER || 'http://ec2-54-144-58-67.compute-1.amazonaws.com/';
+  //URL Server AWS 7 de Diciembre del 2025
+ const URL_SERVER=process.env.URL_SERVER || 'http://ec2-3-148-103-201.us-east-2.compute.amazonaws.com';
  const ENABLE_SSL = true; // Cambia a true para habilitar SSL
 
 /*
